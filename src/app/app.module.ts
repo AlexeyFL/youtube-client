4877f4e44c2875a7251e27946d35af4620721413
AlexeyFL <alexeyfl563@gmail.com>
2021-07-28 17:17:38 +0300

    feat: add components & css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { UserLoginComponent } from './header/user-login/user-login.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchComponent } from './search/search/search.component';
+import { SortComponent } from './sort/sort/sort.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserLoginComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    SearchComponent,
+    SortComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
