692d1822db124d356b3883e5554e564c91dfcba7
AlexeyFL <alexeyfl563@gmail.com>
2021-08-08 11:59:10 +0300

    fix: fix lint errors

--- src/app/header/user-login/user-login.component.ts
+++ src/app/header/user-login/user-login.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.scss']
+  styleUrls: ['./user-login.component.scss'],
 })
-export class UserLoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class UserLoginComponent {}
